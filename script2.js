90e5b45f885c02e47d0451bbb1df96a48da928d5
kaka-jaques <kjfakhouri@gmail.com>
2022-08-02 18:46:08 -0300

    mds, não subi antes...
    Signed-off-by: kaka-jaques <kjfakhouri@gmail.com>

---
+++ script2.js
@@ -0,0 +1,9 @@
+console.warn("READ");
+
+let leitura = document.getElementById("paragrafo_ler");
+
+console.log(leitura);
+
+console.log("Apenas o conteúdo ou textContent",leitura.textContent);
+
+console.log("Apenas o conteúdo + elementos internos com HTML",leitura.innerHTML);
